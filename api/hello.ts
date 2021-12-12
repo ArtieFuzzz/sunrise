20e891acbde17ebaee9e23df6ef3dddefcb5befc
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-12-12 21:53:16 +1100

    Add osu endpoint

--- api/hello.ts
+++
@@ -1,5 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default async (_: VercelRequest, res: VercelResponse) => {
-	return res.send({ message: 'Hello World!' })
-}
---
+++ api/osu.ts
@@ -0,0 +1,15 @@
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import { getUser } from '../lib/osu'
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+	const username = req.query.username as string
+	const userData = await getUser(username)
+
+	if (!userData) {
+		res.statusCode = 404
+
+		return res.send({ error: true, message: 'User not found' })
+	}
+
+	return res.send(userData)
+}
